33969646546356864e8b750ace110f106728113a
tuandat1601 <nguyentuandat1601@gmail.com>
2022-07-06 15:56:05 +0700

    first commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+app.use(express.json())
+app.post("/v1/update", (req,res)=>{
+	setTimeout(()=>{
+			res.status(200).json(req.body)
+	},[2000])
+})
+app.listen(5000, ()=>{
+	console.log("run")
+})
